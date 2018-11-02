30518b4b8850c2302fbb47c21ce82251ab8f78fa
Moses-Hamon <moses_hamon@bigpond.com>
2018-11-02 10:30:14 +1000

    Updated to Framework 4.5.1 to allow for Dapper Added Database connection.

--- YourProjectWebService/Models/Database.cs
+++ YourProjectWebService/Models/Database.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Web;
 
 namespace YourProjectWebService.Models
 {
-    public class Database
+    public static class Database
     {
         private const string DbName = "your_project.db";
+        private static readonly string ConnectionString;
+
+        static Database()
+        {
+            //Gets the path for the database
+            var dbPath = HttpContext.Current.Server.MapPath($"~/App_Data/{DbName}");
+            ConnectionString = $"Data Source={dbPath}";
+        }
+
+        public static SQLiteConnection SqLiteConnection()
+        {
+            return new SQLiteConnection(ConnectionString);
+        }
+
+
     }
 }
