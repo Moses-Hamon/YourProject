0d723e647f0f91b96c41157d4716f69bde0dc7ab
Moses <moses_hamon@bigpond.com>
2018-11-07 11:27:02 +1000

    Added Data annotations for props

--- YourProjectWebApp/Models/Brand.cs
+++ YourProjectWebApp/Models/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,11 @@ namespace YourProjectWebService.Models
     public class Brand
     {
         public int BrandId { get; set; }
+
+        // Annotations are used to apply restrictions which will help with good structure
+        // and allow for input validation when creating the forms.
+        [Required]
+        [StringLength(50)]
         public string BrandName { get; set; }
     }
 }
