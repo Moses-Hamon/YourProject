9236b3c5df515221fb6e8fa31e4b9b9b6b89206d
Moses <moses_hamon@bigpond.com>
2018-11-07 10:46:39 +1000

    Added in Comments and Responses for GetTool Method for API

--- YourProjectWebService/Controllers/Api/ToolController.cs
+++ YourProjectWebService/Controllers/Api/ToolController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 using Dapper;
 using YourProjectWebService.Models;
 
@@ -13,23 +14,54 @@ namespace YourProjectWebService.Controllers.Api
     {
         private const string QuerySelectAll = "SELECT * FROM Tool";
         private const string QuerySelectOne = "SELECT * FROM Tool WHERE ToolId = @id";
-
         private const string QueryInsertInto =
             "INSERT INTO Tool (brandId, description, active, comments, inUse) VALUES (@brandId, @description, @active, @comments, @inUse);";
-
         private const string QueryUpdate =
             "UPDATE Tool SET brandId=@brandId, description=@description, active=@active, comments=@comments, inUse=@inUse WHERE ToolId=@id;";
 
         // GET /api/tool
-        [HttpGet]
+        [System.Web.Http.HttpGet]
         public IEnumerable<Tool> GetAllTools()
         {
             using (var db = Database.GetConnection().OpenAndReturn())
             {
                 return db.Query<Tool>(QuerySelectAll).ToList();
             }
-                
+
         }
+
+        // GET /api/tool/1
+        [System.Web.Http.HttpGet]
+        public HttpResponseMessage GetTool(int id)
+        {
+            //opens the database
+            using (var db = Database.GetConnection().OpenAndReturn())
+            {
+
+                // params allows for more control over the variables used in SQL queries
+                // less likely to suffer from SQL injection
+                var param = new { id };
+
+                // passing the query casted to our tool object into a variable
+                var tool = db.QuerySingleOrDefault<Tool>(QuerySelectOne, param);
+
+                // check to see if the object is null and if so created an error response
+                if (tool == null)
+                {
+                    var errorMessage = $"There are no tools with id = {id}";
+                    var httpError = new HttpError(errorMessage);
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, httpError);
+                }
+                // returns the requested object.
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, tool);
+                }
+                
+            }
+        }
+
+
 
     }
 }
