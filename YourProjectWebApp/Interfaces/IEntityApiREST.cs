44f43c2b8173a88ff6b83617bb6a74d47ecac6ea
Moses <moses_hamon@bigpond.com>
2018-11-11 20:26:27 +1000

    Init for Interface for REST methods

---
+++ YourProjectWebApp/Interfaces/IEntityApiREST.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YourProjectWebApp.Models;
+
+namespace YourProjectWebApp.Interfaces
+{
+    public interface IEntityApiRest
+    {
+
+        Task<IEnumerable<T>> GetAll<T>();
+
+        Task<IEnumerable<T>> GetOne<T>();
+
+
+    }
+}
