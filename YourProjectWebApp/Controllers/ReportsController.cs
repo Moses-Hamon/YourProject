9cd48dab4df87d7b969b9ab91c53d4203727aaa2
Moses <moses_hamon@bigpond.com>
2018-11-18 13:01:21 +1000

    Added Download CSV

--- YourProjectWebApp/Controllers/ReportsController.cs
+++ YourProjectWebApp/Controllers/ReportsController.cs
@@ -17,10 +17,10 @@ namespace YourProjectWebApp.Controllers
             return View();
         }
 
-        
+
         public ActionResult RetrieveReport(string queryType, int? brand)
         {
-            
+
             // open connection
             var svc = new YourProjectServiceSoapClient();
             var viewModel = new ToolWithBrandIndexViewModel
@@ -29,20 +29,31 @@ namespace YourProjectWebApp.Controllers
             };
 
             // store list based on query and brand
-            viewModel.Tools = svc.GetAllToolsWithCondition(brand == null ? SelectQuery(queryType, null) : SelectQuery(queryType, brand));
+            viewModel.Tools = svc.GetAllToolsWithCondition(brand == null 
+                ? SelectQuery(queryType, null)
+                : SelectQuery(queryType, brand));
+
+            // Store the queryName
+            TempData.Add("query", queryType);
+            TempData.Add("brand", brand);
             // return partial view with new info
             return PartialView(viewModel);
         }
 
-        public FileResult DownloadCSV(ToolWithBrandIndexViewModel model)
+        public FileResult DownloadCSV(string queryType, int? brand)
         {
             var builder = new StringBuilder();
-            foreach (var tool in model.Tools)
+            var svc = new YourProjectServiceSoapClient();
+            var model = svc.GetAllToolsWithCondition(brand == null
+                ? SelectQuery(queryType, null)
+                : SelectQuery(queryType, brand));
+
+            foreach (var tool in model)
             {
                 builder.AppendLine(ConvertToolToCsvString(tool));
             }
-
-            return File(new UTF8Encoding().GetBytes(builder.ToString()), "text/csv", "Report.csv");
+            
+            return File(new UTF8Encoding().GetBytes(builder.ToString()), "text/csv", $"{queryType}_{DateTime.Now.ToShortDateString()}_Report.csv");
         }
 
         public string ConvertToolToCsvString(Tool tool)
@@ -60,22 +71,28 @@ namespace YourProjectWebApp.Controllers
             {
                 // Filters
                 // For all checked out tools.
-                case "AllChecked": queryString = " WHERE inUse='1';";
+                case "AllChecked":
+                    queryString = " WHERE inUse='1';";
                     break;
                 // All active tools.
-                case "AllActive": queryString = " WHERE active='1'";
+                case "AllActive":
+                    queryString = " WHERE active='1'";
                     break;
                 // All active tools for a specific brand
-                case "AllActiveByBrand": queryString = $" WHERE active='1' AND brandId='{brandId}';";
+                case "AllActiveByBrand":
+                    queryString = $" WHERE active='1' AND brandId='{brandId}';";
                     break;
                 // All retired tools
-                case "AllRetired": queryString = " WHERE active='0';";
+                case "AllRetired":
+                    queryString = " WHERE active='0';";
                     break;
                 // All retired tools for a specific brand.
-                case "AllRetiredByBrand": queryString = $" WHERE active='0' AND brandId='{brandId}';";
+                case "AllRetiredByBrand":
+                    queryString = $" WHERE active='0' AND brandId='{brandId}';";
                     break;
                 // do not modify the query (defaults to SELECT ALL)
-                default: queryString = "";
+                default:
+                    queryString = "";
                     break;
             }
 
